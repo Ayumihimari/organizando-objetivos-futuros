75f362eee37864210b57bf71dfc46134dfa171bf
Ellen Luiza <137297820+Ayumihimari@users.noreply.github.com>
2025-03-10 08:49:30 -0300

    html

---
+++ main.js
@@ -0,0 +1,43 @@
+<script src="main.js"></script>
+const botoes = document.querySelectorAll(".botao");
+console.log(botoes);
+for (let i = 0; i < botoes.length; i++) {
+    console.log(i);
+  }
+  botoes[i].onclick = function () {
+    botoes[i].classList.add("ativo");
+  };
+  for (let j = 0; j < botoes.length; j++) {
+    botoes[j].classList.remove("ativo");
+  }
+  const botoes = document.querySelectorAll(".botao");
+const textos = document.querySelectorAll(".aba-conteudo");
+
+botoes[i].classList.add("ativo");
+textos[i].classList.add("ativo");
+
+botoes[j].classList.remove("ativo");
+textos[j].classList.remove("ativo");
+
+contadores[0].textContent = calculaTempo(tempoObjetivo1);
+function calculaTempo(tempoObjetivo) {
+    let tempoAtual = new Date();
+    let tempoFinal = tempoObjetivo - tempoAtual;
+  }
+  let segundos = Math.floor(tempoFinal / 1000);
+let minutos = Math.floor(segundos / 60);
+let horas = Math.floor(minutos / 60);
+let dias = Math.floor(horas / 24);
+return (
+    dias +
+    " dias " +
+    horas +
+    " horas " +
+    minutos +
+    " minutos " +
+    segundos +
+    " segundos"
+  );
+  segundos %= 60;
+minutos %= 60;
+horas %= 24;
